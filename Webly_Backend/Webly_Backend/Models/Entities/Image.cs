1116d822e7c0ef37fffe4867560349c2ad04f200
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-12-03 20:05:03 -0500

    Added getters and setters to the attributes from the Image.cs class.

--- Webly_Backend/Webly_Backend/Models/Entities/Image.cs
+++ Webly_Backend/Webly_Backend/Models/Entities/Image.cs
@@ -2,6 +2,12 @@
 {
     public class Image
     {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public User User { get; set; }
+        public DateTime PostingDate { get; set; }
+        public List<Tag> Tags { get; set; }
+ 
 
     }
 }
