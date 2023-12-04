1b5fa3e1a61678e8fa68d1edf7b62f6215fe3173
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-12-03 20:08:21 -0500

    Added getters and setters to the attributes from the User.cs class.

--- Webly_Backend/Webly_Backend/Models/Entities/User.cs
+++ Webly_Backend/Webly_Backend/Models/Entities/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public List<Image> Images { get; set; }
+
     }
 }
