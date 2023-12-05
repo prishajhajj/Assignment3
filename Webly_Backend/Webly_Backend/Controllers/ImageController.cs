7aa567959f57799eaf83fdbc2c8105c640bc7cf2
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-12-04 19:26:16 -0500

    Added the Get Images method with the HttpGet tag added on top of the GetImages GET method.

--- Webly_Backend/Webly_Backend/Controllers/ImageController.cs
+++ Webly_Backend/Webly_Backend/Controllers/ImageController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Webly_Backend.Models.Database;
 
 namespace Webly_Backend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/image")]
     [ApiController]
     public class ImageController : ControllerBase
     {
+        private readonly AppDbContext dbContext;
+        
+        public ImageController(AppDbContext dbContext)
+        {
+                
+        }
+
+
+        [HttpGet]
+        public IActionResult GetImages( )
+        {
+            return Ok(dbContext.Images.ToList());
+        }
     }
 }
--- Webly_Backend/Webly_Backend/Program.cs
+++ Webly_Backend/Webly_Backend/Program.cs
@@ -2,20 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using Webly_Backend.Models.Database;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("YourConnectionName")));
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("ImageDb"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+
+app.MapGet("/images", () =>
+{
+   
+})
+.WithName("GetWeatherForecast");
 
 app.UseHttpsRedirection();
 
