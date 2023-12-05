d628b692eab27f16efeac0ef707aa501d49a35ca
Prisha Jhajj <jhajjpr@sheridancollege.ca>
2023-12-04 22:38:26 -0500

    Added the app.MapGet method including the /api/images endpoint and app.MapPost method which includes the /api/images endpoint, in the Program.cs file.

--- Webly_Backend/Webly_Backend/Program.cs
+++ Webly_Backend/Webly_Backend/Program.cs
@@ -1,15 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using Webly_Backend.Models.Database;
-
 var builder = WebApplication.CreateBuilder(args);
-
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("ImageDb"));
 
 var app = builder.Build();
 
@@ -20,20 +14,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
-app.MapGet("api/images", () =>
-{
-   
-})
-.WithName("GetWeatherForecast");
-
-app.MapPost("api/images", () =>
-{
-
-});
-
 app.UseHttpsRedirection();
 
 
+
+app.MapGet("/api/images", () =>
+{
+})
+.WithName("GetWeatherForecast");
+
+app.MapPost("/api/images", () =>
+{
+});
+
 app.Run();
+
 
--- Webly_Backend/Webly_Backend/Response/PageResponse.cs
+++ Webly_Backend/Webly_Backend/Response/PageResponse.cs
@@ -1,6 +1,15 @@
 ﻿namespace Webly_Backend.Response
 {
-    public class PageResponse
+    public class PageResponse<T>
     {
+        public List<T> Data { get; set; }
+
+        public Dictionary<string, int> Meta { get; set; } = new Dictionary<string, int>();
+        public Dictionary<string, string> Links { get; set; } = new Dictionary<string, string>();
+
+        public PageResponse(List<T> data)
+        {
+            Data = data;
+        }
     }
 }
